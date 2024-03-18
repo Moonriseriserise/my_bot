5a62f03edadee43cd39ceffece0daf201d96c963
riseriserise <moonriseriserise@gmail.com>
2024-03-18 14:32:26 +0700

    Запуск сразу двух ботов

--- main.py
+++ main.py
@@ -14,23 +14,40 @@ ALLOW_UPDATE = ["message, edited_message"]
 client_bot = Bot(token=os.getenv("CLIENTTOKEN"),)
 admin_bot = Bot(token=os.getenv("ADMINTOKEN"),)
 
-dp = Dispatcher()
+client_dp = Dispatcher()  # КЛИЕНТ
+admin_dp = Dispatcher()  # АДМИН
 
 
-@dp.message(Command("admin"))
+@client_dp.message(Command("start"))
 async def send_welcome(message: types.Message):
+    print("Отработал клиент")
+
     await message.reply("Привет! Я клиент бот.")
 
 
-@dp.message(Command("admin"))
+@admin_dp.message(Command("start"))
 async def send_welcome(message: types.Message):
+    print("Отработал админ")
+
     await message.reply("Привет! Я админ бот.")
 
 
-async def main():
+async def client():
+    await client_bot.delete_webhook(drop_pending_updates=True)
+    await client_dp.start_polling(client_bot, allowed_updates=ALLOW_UPDATE)
+    #   await admin_dp.start_polling(admin_bot, allowed_updates=ALLOW_UPDATE)
 
-    await dp.start_polling(client_bot, allowed_updates=ALLOW_UPDATE)
-    await dp.start_polling(admin_bot, allowed_updates=ALLOW_UPDATE)
+
+async def admin():
+    await admin_bot.delete_webhook(drop_pending_updates=True)
+    await admin_dp.start_polling(admin_bot, allowed_updates=ALLOW_UPDATE)
+
+
+async def main():
+    task_client = asyncio.create_task(client())
+    task_admin = asyncio.create_task(admin())
+
+    await asyncio.gather(task_client, task_admin)
 
 
 asyncio.run(main())
